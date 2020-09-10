99a067af02b3680dcaa543baeabe33531ada8df6
Christopher Kerr <christopherke@truck-hero.com>
2020-09-09 21:59:57 -0400

    chris

---
+++ SafeTripTravelCompanion/SafeTripTravelCompanion/Models/DataBase/Profiles/Profile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SafeTripTravelCompanion.Models.DataBase.Profiles
+{
+    public class Profile
+    {
+    }
+}
