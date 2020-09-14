441a412494de2a46cde746a4e8f75fcb292632f4
David Al-Yakobi <51806013+dalyakob@users.noreply.github.com>
2020-09-14 19:16:08 -0400

    fixed search view

--- SafeTripTravelCompanion/SafeTripTravelCompanion/Controllers/TripAdvisorController.cs
+++ SafeTripTravelCompanion/SafeTripTravelCompanion/Controllers/TripAdvisorController.cs
@@ -19,7 +19,7 @@ namespace SafeTripTravelCompanion.Controllers
         // GET: TripAdvisorController1
         public async Task<ActionResult> Index(string search)
         {
-            ViewBag.UserInput = search;
+            ViewBag.Search = search;
             var model = await _tripAdvisorService.GetLocation(search);
             return View(model.data);
         }
---
+++ SafeTripTravelCompanion/SafeTripTravelCompanion/Data/Migrations/20200914222726_NewTableName.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SafeTripTravelCompanion.Data.Migrations
+{
+    public partial class NewTableName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
